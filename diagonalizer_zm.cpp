d285a209608eedd3bb8f1a3dcb7fa8f6b80138ec
Felix Boes <felixboes@gmail.com>
2013-10-04 21:43:15 +0200

    Code lesbarer gemacht.

--- diagonalizer_zm.cpp
+++ diagonalizer_zm.cpp
@@ -42,7 +42,7 @@ uint32_t DiagonalizerZm::diag_field(MatrixZm &matrix)
     size_t num_cols = matrix.size2();
     size_t rank = 0; // Stores the dimension of the subspace spanned by the first 0 \le j < col_c columns. The dimension is at most of size row_c.
     
-    /// @TODO: Is #rows < #cols?
+    /// @TODO: Is #rows <= #cols?
     // if( num_rows <= num_cols )
     {
         // Wir gehen davon aus, dass die Anzahl der Zeilen echt kleiner als die Anzahl der Spalten ist.
@@ -67,29 +67,31 @@ uint32_t DiagonalizerZm::diag_field(MatrixZm &matrix)
         // Iterate through columns. We may stop if the rank is maximal.
         for ( size_t col = 0; col < num_cols && rank < num_rows; col++ )
         {
-            bool done = false;  
-            for( auto it = rows_to_check.begin(); !done && it != rows_to_check.end(); ++it )
+            // Find first non-zero entry in the remaining rows.
+            auto it = rows_to_check.begin();
+            for( ; it != rows_to_check.end() && matrix( *it, col ).is_invertible() == false; ++it )
             {
+            }
+            
+            // Does this row contribute to the rank, i.e. did we find an invertable element?
+            if( it != rows_to_check.end() )
+            {
+                // This row contributes to the rank.
+                rank++;
                 size_t row_1 = *it;
-                // Is matrix(row_1,col) an invertible element in Zm?
-                if( matrix( row_1, col ).is_invertible() )
+                // We do not need to check this row in further iterations.
+                // After erasing, it will point to the next element in the list.
+                it = rows_to_check.erase(it);
+                // Use row operations to zeroise the remaining elements of the column.
+                for( ; it != rows_to_check.end(); ++it )
                 {
-                    it = rows_to_check.erase(it);
-                    done = true;
-                    rank++;
-                    // matrix operations
-                    // it will point to the next element in the list
-                    for( ; it != rows_to_check.end(); ++it )
+                    size_t row_2 = *it;
+                    // Assume that the entry (row_2, col) differs from zero.
+                    // Performs a row operation to matrix to zeroise the entry (row_2, col) 
+                    // using the entry (row_1, col). 
+                    if( matrix( row_2, col ) )
                     {
-                        size_t row_2 = *it;
-                        // Assume that the entry (row_2, col) differs from zero.
-                        // Performs a row operation to matrix to zeroise the entry (row_2, col) 
-                        // using the entry (row_1, col). 
-                        if( matrix( row_2, col ) )
-                        {
-                            row_operation( matrix, row_1, row_2, col );
-                            std::cout << matrix << std::endl;
-                        }
+                        row_operation( matrix, row_1, row_2, col );
                     }
                 }
             }
