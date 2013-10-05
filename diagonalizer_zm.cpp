f477cdfb5c2b6338100b85290fbf658e49eca8e6
JupiterLux <anhermann@online.de>
2013-10-05 11:00:39 +0000

    Kommentare erweitert

--- diagonalizer_zm.cpp
+++ diagonalizer_zm.cpp
@@ -34,27 +34,22 @@ void DiagonalizerZm::row_operation(MatrixZm & matrix, size_t row_1, size_t row_2
 
 /**
  *  Compute the dimension of the image of matrix.
- *  This done by computing the number of lineary independant columns or rows.
+ *  This done by computing the number of lineary independent columns or rows.
  */ 
 uint32_t DiagonalizerZm::diag_field(MatrixZm &matrix)
 {
     size_t num_rows = matrix.size1();
     size_t num_cols = matrix.size2();
-    size_t rank = 0; // Stores the dimension of the subspace spanned by the first 0 \le j < col_c columns. The dimension is at most of size row_c.
+    size_t rank = 0; // Stores the dimension of the subspace spanned by the first 0 \le j < col columns. The dimension is at most of size row.
     
     /// @TODO: Is #rows <= #cols?
     // if( num_rows <= num_cols )
     {
-        // Wir gehen davon aus, dass die Anzahl der Zeilen echt kleiner als die Anzahl der Spalten ist.
-        //
-        // Um Dimension des Kerns und des Bildes zu berechnen, reicht es aus,
-        // die Anzahl der linear unabhängigen Zeilen zu bestimmen.
-        //
-        // Wir können im Stufenform-Algorithmus Vertauschungen zu vermeiden:
-        // - Iteriere durch die Spalten.
-        //   - Iteriere durch einfach verkettete Liste der Zeilen.
-        //   - Lösche Eintrag, falls es in dieser Zeile (bei gegebener Spalte) ein invertierbares Element gibt.
-        //   - Mache Zeilenop auf alle vorhandenen Einträge.
+        // We avoid permutations of rows in the Gauss algorithm by performing the following steps:
+        //   - Iterate through the columns.
+        //   - Iterate through a singly linked list of rows.
+        //   - For a given column, remove a row from the list if the common entry of column and row is invertible.
+        //   - Apply row operations to all rows beneath the row.
         
         std::list<size_t> rows_to_check;
         
@@ -67,13 +62,13 @@ uint32_t DiagonalizerZm::diag_field(MatrixZm &matrix)
         // Iterate through columns. We may stop if the rank is maximal.
         for ( size_t col = 0; col < num_cols && rank < num_rows; col++ )
         {
-            // Find first non-zero entry in the remaining rows.
+            // Find first invertible (i.e. non-zero) entry in the remaining rows.
             auto it = rows_to_check.begin();
             for( ; it != rows_to_check.end() && matrix( *it, col ).is_invertible() == false; ++it )
             {
             }
             
-            // Does this row contribute to the rank, i.e. did we find an invertable element?
+            // Does this row contribute to the rank, i.e. did we find an invertible element?
             if( it != rows_to_check.end() )
             {
                 // This row contributes to the rank.
@@ -82,13 +77,12 @@ uint32_t DiagonalizerZm::diag_field(MatrixZm &matrix)
                 // We do not need to check this row in further iterations.
                 // After erasing, it will point to the next element in the list.
                 it = rows_to_check.erase(it);
-                // Use row operations to zeroise the remaining elements of the column.
+                // Use row operations to zeroize the remaining elements of the column.
                 for( ; it != rows_to_check.end(); ++it )
                 {
                     size_t row_2 = *it;
-                    // Assume that the entry (row_2, col) differs from zero.
-                    // Performs a row operation to matrix to zeroise the entry (row_2, col) 
-                    // using the entry (row_1, col). 
+                    // Assuming that the entry (row_2, col) differs from zero, we perform
+                    // a row operation to matrix to zeroize the entry (row_2, col) using the entry (row_1, col). 
                     if( matrix( row_2, col ) )
                     {
                         row_operation( matrix, row_1, row_2, col );
