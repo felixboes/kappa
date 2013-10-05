0f431062ebe4d4991e00de41a56c4f787fd6b16b
JupiterLux <anhermann@online.de>
2013-10-05 11:08:41 +0000

    per Hand Änderungen gemergt (meine Kommentare übernommen und deine Includelöschungen vorgenommen)

--- diagonalizer_zm.hpp
+++ diagonalizer_zm.hpp
@@ -13,27 +13,35 @@
  *  \f[
  *          C_{n-1} \xleftarrow{\partial_{in}} C_n \xleftarrow{\partial_{out}} C_{n-1}
  *  \f]
- *  at position \f$n\f$, it suffices to compute the dimension of the kernel and the image of the respective matrices.
+ *  at position \f$n\f$, it suffices to compute the dimension of the kernel and the image of the respective matrices. 
+ *  Therefore we need to diagonalize the matrices representing the two differentials (here only for matrices with
+ *  coefficients in Z/m).
  */
 
 class DiagonalizerZm
 {
 public:
-    typedef std::vector< Zm > TorsionVector;
+    typedef std::vector< Zm > TorsionVector;  
+    
+    /**  constructor */
     DiagonalizerZm(MatrixZm &out_differential, MatrixZm &in_differential );
+    
+    /**  \return defect of the matrix out */
     uint32_t defect() {return def;}
+    
+    /**  \return vector of torsion coefficients in the homology of C_n (with the above notation) */
     TorsionVector torsion_coefficients() {return tors_coefficients;}
     
 private:
     
     /**
-     *  Performs a row operation to matrix to zeroise the entry (row_2, col) using the entry (row_1, col).
+     *  Performs a row operation to matrix to zeroize the entry (row_2, col) using the entry (row_1, col).
      */
     void row_operation(MatrixZm &matrix, size_t row_1, size_t row_2, size_t col);
     
     /**
-     *  Compute the dimension of the image of matrix.
-     *  This done by computing the number of lineary independant columns or rows.
+     *  \return rank of matrix
+     *  The matrix is diagonalized via Gauss to compute the number of linearly independant columns or rows.
      */
     uint32_t diag_field(MatrixZm& matrix);
     
