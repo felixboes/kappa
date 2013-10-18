1fd364d45a7449b8f8ca0529a478180e3973b7f4
Felix Boes <felixboes@gmail.com>
2013-10-18 23:39:21 +0200

    Beginne altes Kappa-Projekt zu integrieren.

---
+++ factorial.hpp
@@ -0,0 +1,8 @@
+#ifndef FACTORIAL_HPP
+#define FACTORIAL_HPP
+
+#include <cinttypes>
+
+uint64_t factorial( uint32_t k  ); ///< @return \f$ k! \f$ for \f$ k \le 20 \f$ since \f$ 21! > 2^64 - 1 \$f.
+
+#endif // FACTORIAL_HPP
