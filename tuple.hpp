68a19be908dcccaa4ac423df0c48317620851529
JupiterLux <anhermann@online.de>
2013-11-26 19:06:39 +0000

    Add comments.

--- tuple.hpp
+++ tuple.hpp
@@ -21,13 +21,13 @@ struct PermutationType
  *  A Tuple of Transpositions \f$ ( \tau_h \mid \ldots \mid \tau_1 ) \f$.
  *  
  *  Convention: We want an easy-to-check condition, to test if a Tuple is in a bad state.
- *  We require, that good Tuples use only the symbols \f$ 1 \f$ to \f$ p \f$.
+ *  We require that good Tuples use only the symbols \f$ 1 \f$ to \f$ p \f$.
 **/
 class Tuple
 {
 public:
     /**
-     *  Construct a Tuple of norm h, which has to be filled.
+     *  Construct a Tuple of norm h which has to be filled.
      */
     Tuple(size_t h = 1);
     
@@ -65,23 +65,23 @@ public:
     friend std::ostream& operator<< (std::ostream& stream, const Tuple& tuple);
     
     /**
-     *  Gibt an, wieviele Zykel / interne Punktierungen die Permutation \f$ \tau_h \cdot \ldots \cdot \tau_1 \cdot (1,2,\ldots,p) \f$ hat.
+     *  Tells the number of cycles and intern punctures of the permutation \f$ \tau_h \cdot \ldots \cdot \tau_1 \cdot (1,2,\ldots,p) \f$.
      */
     PermutationType permutation_type();
     
-    bool monoton();                     ///< Gibt genau dann true zurueck, wenn das Tupel monoton ist.
-    bool f(uint32_t i);                 ///< Die Funktion \f$ f_i \f$ fuer \f$ 1 \le i < h \f$.
-    bool phi( uint32_t q, uint32_t i);   ///< Die Funtkion \f$ \Phi^q_i \f$.
-    Tuple d_hor( uint8_t i ) const;         ///< Der i-te horizontale Rand \f$ \partial_i^{\prime \prime}
-    Tuple d_hor_test( uint8_t i ) const;    ///< Different implementation.
+    bool monotone();                        ///< Returns true iff the tuple is monotone.
+    bool f(uint32_t i);                     ///< Applies the function \f$ f_i \f$ fuer \f$ 1 \le i < h \f$ and returns true iff the norm is preserved thereby.
+    bool phi( uint32_t q, uint32_t i);      ///< Applies the function \f$ \Phi^q_i \f$ and returns true iff the norm is preserved thereby..
+    Tuple d_hor( uint8_t i ) const;         ///< Applies the i-th horizontal boundary  \f$ \partial_i^{\prime \prime} and returns true iff the norm is preserved thereby.
+    Tuple d_hor_test( uint8_t i ) const;    ///< Different implementation of the i-th horizontal boundary.
     
     uint32_t p;  ///< The number of symbols \f$ 1 \le p \f$ to be permuted.
 private:
     Transposition& at(size_t);
-    std::vector< Transposition > rep;   ///< Representation eines Tuples von Transpositionen.
-    typedef std::map<uint8_t, uint8_t> Permutation;
-    Permutation long_cycle() const;
-    Permutation long_cycle_inv() const;
+    std::vector< Transposition > rep;                ///< Representation of a tuple of transpositions.
+    typedef std::map<uint8_t, uint8_t> Permutation;  ///< Data structure to store a permutation, where a -> perm[a] if perm is a Permutation
+    Permutation long_cycle() const;                  ///< Returns the cycle 1 -> 2 -> ... -> p-1 -> p -> 1.
+    Permutation long_cycle_inv() const;              ///< Returns the cycle 1 -> p -> p-2 -> ... -> 2 -> 1. 
 };
 
 /// output stream
