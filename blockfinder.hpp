b6701b3e0baea9488f6b5c634ecea88078837dc9
JupiterLux <anhermann@online.de>
2014-02-22 08:53:36 +0000

    Kommentiere mehr

--- blockfinder.hpp
+++ blockfinder.hpp
@@ -6,18 +6,27 @@
 
 #include "tuple.hpp"
 
-typedef std::vector< int > Component;
-typedef std::pair< Component, Component > Block;
-typedef std::vector< Block > BlockPartition;
+typedef std::vector< int > Component; ///< used to store a a subset of rows, or of columns of a matrix 
+typedef std::pair< Component, Component > Block; ///< used to store a block of a matrix
+typedef std::vector< Block > BlockPartition; ///< used to store a partition of the matrix into blocks
 
+/**
+ * @brief Determines the block structure of a given matrix of type MatrixT.
+ * Thereby a block of a matrix is defined as a maximal collection C of rows and columns
+ * of the matrix such that for each row r and each column c, there is an alternating 
+ * sequence of rows and columns in C starting at r and ending at c.
+ */
 template< class MatrixT >
 class BlockFinder
 {
 public:
     typedef MatrixT MatrixType;
+    
     /**
-     * Determines a block partition of the given matrix and stores it in _block_part.
+     * Determines a block partition of the given matrix, i.e. a collection of blocks
+     * covering all rows of the matrix, and stores it in _block_part. 
      * @warning 0-rows build own blocks in the block partition.
+     * @warning 0-columns are ignored in the block partition.
      */
     BlockFinder(MatrixType &);
 
@@ -25,18 +34,21 @@ public:
      * @return _block_part
      */
     BlockPartition & block_partition();
+    
     /**
      * @return Total number of blocks in _block_part.
+     * @warning 0-rows build own blocks in the block partition.
+     * @warning 0-columns are ignored in the block partition.
      */
     int num_blocks() const;
 
     /**
-     * @return Number of blocks in _block_part which are not zero rows.
+     * @return Number of blocks in _block_part which are not zero rows or zero blocks.
      */
     int num_non_zero_blocks() const;
 
 private:
-    BlockPartition _block_part;
+    BlockPartition _block_part; ///< stores a block partition of the given matrix
 };
 
 #include "blockfinder.ipp"
