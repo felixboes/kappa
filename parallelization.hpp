2a45cd145e18ba29f8f11073a9dfa2e1ce30e791
Felix Boes <felixboes@gmail.com>
2014-02-26 16:43:04 +0100

    Unwichtiger Schreibfehler;

--- parallelization.hpp
+++ parallelization.hpp
@@ -14,7 +14,7 @@
 typedef std::atomic<uint32_t> atomic_uint;
 
 /**
- *  In order to work with several g++-versions that do not yet support full c++11 namely g++ <= 4.6 on ubuntu 12.04 or c++ > 4.6,
+ *  In order to work with several g++-versions that do not yet support full c++11 namely g++ <= 4.6 on ubuntu 12.04 or g++ > 4.6,
  *  you can use this function to see wether a thread is still running or not.
  *  @returns true iff the thread is still running.
  */
