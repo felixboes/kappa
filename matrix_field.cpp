8aa9a0483979c6b5a57f529bb4e190bf31626a4e
Anna Hermann <anhermann@online.de>
2014-04-22 19:56:08 +0200

    Implement matrix for boolean coefficients

---
+++ matrix_field.cpp
@@ -0,0 +1,47 @@
+#include "matrix_field.hpp"
+
+MatrixBool::MatrixBool() : data(), num_cols(0), num_rows(0)
+{
+}
+
+MatrixBool::MatrixBool( size_t number_rows, size_t number_cols ) : data( number_rows, boost::dynamic_bitset<>(num_cols, 0) ), num_rows(number_rows), num_cols(number_cols)
+{
+}
+
+void MatrixBool::row_operation( size_t row_1, size_t row_2, size_t col )
+{
+    data[row_2] ^= data[row_1];
+}
+
+ bool MatrixBool::operator()( size_t i, size_t j )
+ {
+     return data[i][j];
+ }
+
+ const bool MatrixBool::at(size_t i, size_t j) const
+{
+    return data[i][j];
+}
+
+size_t MatrixBool::size1() const
+{
+    return num_rows;
+}
+
+size_t MatrixBool::size2() const
+{
+    return num_cols;
+}
+
+void MatrixBool::clear()
+{
+    for( auto & it : data )
+    {
+        boost::dynamic_bitset<> & row = it;
+        for ( size_t j = 0; j < num_cols; ++j)
+        {
+            row[j] = 0;
+        }
+    }
+}
+
--- matrix_field.hpp
+++ matrix_field.hpp
@@ -8,6 +8,7 @@
 //
 // Moreover this header offers matrices with Q and Zm coefficients, namely MatrixQ and MatrixZm.
 
+#include <boost/dynamic_bitset.hpp>
 #include <iostream>
 #include <vector>
 
@@ -116,6 +117,99 @@ template class MatrixField<Zm>;
 
 typedef MatrixField<Q> MatrixQ;     ///< This defines Matrices with \f$\mathbb Q\f$ coefficients.
 typedef MatrixField<Zm> MatrixZm;   ///< This defines Matrices with \f$\mathbb Z/ m\mathbb Zf$ coefficients.
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class MatrixBool
+{
+public:
+    typedef std::vector<boost::dynamic_bitset<> > MatrixStorageType;    ///< This realizes the implementation of the data.
+
+    MatrixBool();  ///< Creates a \f$ 0 \times 0\f$ matrix.
+    /**
+     *  Creates a matrix with num_rows rows and num_cols columns.
+     *  The entries are determined by the standard constructor of CoefficientT.
+     *  @warning If the standard constructor of CoefficientT does not create a coefficient with value zero (e.g. usind standard int types)
+     *  the result differ from your imagination.
+     *  In order to get a zero matrix you may use clear();
+     */
+    MatrixBool( size_t number_rows, size_t number_cols );  ///<
+
+    /**
+     *  This performs a row operation in the Gauss algorithm.
+     *  The entry in (row_1, col) is the given entry that is used to erase the entry in (row_2, col).
+     */
+    void row_operation( size_t row_1, size_t row_2, size_t col );
+
+    /**
+     *  In order to access elements of the matrix you want to use this function.
+     *  @return The function returns a reference to the given entry.
+     *  @todo throw an exception if necessary i.e. if (i,j) is not a valid entry.
+     */
+    bool operator()( size_t i, size_t j );
+
+    /**
+     *  As our implementation mimes ublas::matrix we use the same (awkward) method to delete a matrix.
+     *  In order to do so call resize(0,0);
+     */
+    void resize (size_t size1, size_t size2, bool preserve = false)
+    {
+        data.clear();
+        num_rows = size1;
+        num_cols = size2;
+        data = MatrixStorageType( size1, boost::dynamic_bitset<> (size2, 0) );
+    }
+
+    size_t size1() const;   ///< @returns the number of rows.
+    size_t size2() const;   ///< @returns the number of columns.
+
+    void clear();   ///< Fills every entry with CoefficientT(0);
+
+     /**
+     *  grant std::ostream access in order to print coefficients to ostreams like 'std::cout << Zm(44) << std::endl;'
+     */
+    friend std::ostream& operator<< ( std::ostream& stream, const MatrixBool & matrix)
+    {
+        for( size_t i = 0; i < matrix.num_rows; ++i )
+        {
+            for( size_t j = 0; j < matrix.num_cols; )
+            {
+                stream << matrix.at(i,j);
+                if( ++j < matrix.num_cols )
+                {
+                    stream << ",";
+                }
+                else
+                {
+                    stream << std::endl;
+                }
+            }
+        }
+        return stream;
+    }
+
+private:
+    /**
+     *  In order to keep constness, we go the usual way and implement the function at.
+     *  You may want to take a look at the at()-methods of the standard containers like std::vector.
+     */
+    const bool at( size_t i, size_t j ) const;
+
+    MatrixStorageType data; ///< This realizes the data.
+    size_t num_rows;    ///< The number of rows.
+    size_t num_cols;    ///< The number of columns.
+
+    /**
+     *  In order to save Zm coefficients we have to grad boost::serialization::access access.
+     */
+    friend class boost::serialization::access;
+
+    template <class Archive>
+    void serialize(Archive &ar, const unsigned int version) ///< Implements the serialization.
+    {
+        ar & num_rows & num_cols & data;
+    }
+};
 
 #include "matrix_field.ipp"
 #endif // MATRIX_FIELD_HPP
