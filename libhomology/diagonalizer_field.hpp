6c1796789fbb4f508ab176ee6aad78674c5070c7
Anna Hermann <anhermann@online.de>
2014-05-19 20:07:37 +0200

    First attempt to improve parallelized diagonalizing.

--- libhomology/diagonalizer_field.hpp
+++ libhomology/diagonalizer_field.hpp
@@ -32,7 +32,8 @@ template < class MatrixT >
 class DiagonalizerField
 {
 public:
-     typedef MatrixT MatrixType;    ///< We use this typedef to grant access the matrix type from other classes.
+    typedef MatrixT MatrixType;    ///< We use this typedef to grant access the matrix type from other classes.
+    class Worker;
 
     /**
      *  Constructor.
@@ -85,62 +86,112 @@ public:
 
     // Classes for paralellization:
     // Todo: give a detailed explanation.
-    /**
-     *   A RowOpParam can be seen as a job that has to be processed by one of the worker threads.
-     */
-    struct RowOpParam
-    {
-        RowOpParam(size_t r1 = 0, size_t r2 = 0, size_t c = 0) : row1(r1), row2(r2), col(c) {}
-        size_t row1;
-        size_t row2;
-        size_t col;
-    };
 
     /**
      *  A JobQueue keeps track of the work that has to be done.
-     *  It can fill itself with jobs i.e. RowOpParam. Furthermore it can tell a
-     *  thread which chunk of jobs are to be performed by this thread.
+     *  This work consists of:
+     *  - A column and a row s.t. the corresponding matrix entry is non-zero.
+     *    With this row, we want to erase all other entries in this column
+     *    that do not belong to rows that already contribute to the rank.
+     *  - A previously computed vector of all rows on which such
+     *    row operations have to be performed.
+     *  - A vector of the remaining rows of the matrix which do not yet
+     *    contribute to the rank.
+     *  When a new column is considered, the JobQueue can update this information.
+     *  This assumes that the workers finish their work before.
      */
     class JobQueue
     {
     public:
-        //! Basic constructors
-        JobQueue(MatrixType & matrix_init, uint32_t number_of_working_threads );
-
+        //! Collect initial work.
+        JobQueue(MatrixType & matrix_init, uint32_t number_of_working_threads, atomic_uint & current_rank );
 
 #ifdef BROKEN_VECTOR_IMPLEMENTATION
         JobQueue(JobQueue const & other);
         void operator=(JobQueue const & other);
 #endif
 
-        //! Access operation with a given id
-        RowOpParam const & get_operation(size_t op_id) const;
+        /**
+         * This function is supposed to be called by update_rank_and_work.
+         * It fetches the all work for the next column computed by the threads
+         * and unites it to become the current work.
+         * ToDo move into c file
+         */
+        void add_up_rows
+            (std::vector<Worker> & workers,
+             std::vector<size_t> & former_rows_to_work_at,
+             std::vector<size_t> & former_remaining_rows);
 
-        //! Get the chunk [begin, end) of operations dedicated to the given thread
+        /**
+         * This function is supposed to be called after all threads finish their work.
+         * It fetches the all work for the next column computed by the threads
+         * and unites it to become the current work.
+         * It also sets the new row_1, rank and chunk size accordingly.
+         * @return True iff there is work left.
+         */
+        bool update_rank_and_work(atomic_uint & current_rank, std::vector<Worker> & workers);
+
+        //! Get the chunk [begin, end) of rows_to_work_at dedicated to the given thread.
         void get_chunk(uint32_t const thread_id, size_t & begin, size_t & end) const;
 
-        /*! \brief Computes new operations and returns whether there is new work
-         *
-         *  The former operations will be purged.
-        **/
-        bool compute_operations(atomic_uint & cur_rank);
-
-    private:
-        //! Recomputes the chunk size based on the current number of operations
+        //! Recomputes the chunk size based on the current number of rows_to_work_at.
         void recompute_chunk_size();
 
         MatrixType &              matrix;
-        std::vector<RowOpParam>   operations;
+        /*!
+         * Rows of the matrix on which row operations w.r.t. col and row_1
+         * have to be performed.
+         */
+        std::vector<size_t>       rows_to_work_at;
+        /*!
+         * All rows of the matrix that do not yet contribute to the rank
+         * and that are not within the vector rows_to_work_at.
+         */
 
+        std::vector<size_t>       remaining_rows;
+
+        /*!
+         * Number of rows in the vector rows_to_work_at
+         * a single thread has to work at.
+         */
         size_t                    cur_chunk_size;
+        /*!
+         * Total number of threads used for parallelization.
+         * One thread takes care of the remaining_rows while all the others
+         * perform row operations on the rows_to_work_at.
+         * TODO Check whether this can be improved!
+         */
         size_t                    number_of_threads;
-
-        std::list<size_t>         rows_to_check; //!< Auxiliary for recompute_chunk_size
+        /*!
+         * Row which is currently added to other rows for row operations.
+         */
+        size_t                    row_1;
+        /*!
+         * Current column.
+         */
         size_t                    col;
     };
 
     /**
-     *  A Worker waits for row operation jobs and performs those.
+     * A Worker is supposed to perform row operations for a given column and to determine
+     * the row operations for the next column.
+     * More precisely:
+     * All Workers maintain a JobQueue of their current shared work.
+     * At initialization, the JobQueue consists of
+     *  - the 0th column of the matrix,
+     *  - the first row for which the entry (col, row_1) is non-zero,
+     *  - the set of non-zero rows rows_to_work_at of the 0th column (except for row_1),
+     *  - the set of remaining rows remaining_rows of the 0th column (except for row_1).
+     * Now, a Worker can either
+     *  - perform the function work and thus performs row operations on a
+     *    part of the rows_to_work_at, and thereby sort these rows into two new vectors,
+     *    depending on whether their entry in the next column is zero or not,
+     *  - sort the remaining_rows into two new vectors,
+     *    depending on whether their entry in the next column is zero or not.
+     * The Workers must be distributed s.t. all rows are considered.
+     * Afterwards, the function jobs.update_rank_and_work collects the rows
+     * computed by the Workers, and they can continue their work until there is
+     * no work left.
      */
     class Worker
     {
@@ -151,14 +202,33 @@ public:
         Worker( Worker const & other);
         void operator=(Worker const & other);
 #endif
+        uint32_t get_id()
+        {
+            return id;
+        }
 
-
-        //! Perform the thread dedicated work. (As determined by the JobQueue.)
+        /*!
+         * Perform row operations on the corresponding chunk of the
+         * rows_to_work_at, and thereby sort these rows into two new vectors,
+         * depending on whether their entry in the next column is zero or not.
+         */
         void work(MatrixType & matrix);
 
+        /*!
+         * Sort the remaining_rows into two new vectors,
+         * depending on whether their entry in the next column is zero or not.
+         */
+        void collect_remaining_work(MatrixType & matrix);
+
+        std::vector<size_t> & get_new_rows_to_work_at();
+
+        std::vector<size_t> & get_new_remaining_rows();
+
     private:
-        uint32_t   id;
-        JobQueue & jobs;
+        uint32_t            id;
+        JobQueue &          jobs;
+        std::vector<size_t> new_rows_to_work_at;
+        std::vector<size_t> new_remaining_rows;
     };
 };
 
