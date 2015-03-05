d7a4dee207133e3c87dd77001ed8a88710b67421
Felix Boes <felixboes@gmail.com>
2015-03-05 11:21:23 +0100

    Lösche unnötige Memberfunktion

--- kappa/doublecomplexbasis.hpp
+++ kappa/doublecomplexbasis.hpp
@@ -34,9 +34,6 @@ struct DoubleComplexBasis
     /// Returns the index of the HighCell that is stored in the Basis or -1.
     int64_t id_of( const HighCell& t ) const;
     
-    /// Returns the index of the HighCell associated to the Tuple that is stored in the Basis or -1.
-    int64_t id_of( const Tuple& t ) const;
-    
     /// Stores the orderd basis.
     std::set< HighCell > basis_red;
     std::set< HighCell > basis_col;
